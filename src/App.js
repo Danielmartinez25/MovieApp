acaacc0b6eeb93d57df3e46c4f7d4b9e331264c1
Danimartinez25 <danimartinez1325@gmail.com>
2023-03-22 16:09:11 -0300

    Result movies

--- src/App.js
+++ src/App.js
@@ -21,32 +21,72 @@ function App() {
   const fetchMovies = async (searchKey) => {
     const type = searchKey ? "search" : "discover";
     const {
-      data: { result },
+      data: { results },
     } = await axios.get(`${API_URL}/${type}/movie`, {
       params: {
         api_key: API_KEY,
         query: searchKey,
       },
     });
-    setMovies(result);
-    setMovie(result[0]);
+    setMovies(results);
+    setMovie(results[0]);
+    if(results.length){
+      await fetchMovie(results[0].id)
+    }
   }
-  useEffect(() =>{
-    fetchMovies();
-  },[])
-  return (
-  <div className="container mt-3">
-    <div className="row">
-    {movie.mop((movie)=>(
-      <div key={movie.id} className="col-md-4 mb-3">
-        <img src={`${URL_IMAGE + movie.poster_path}`} alt="" height={600} width="100"/>
-        <h4 className="text-center">{movie.title}</h4>
-      </div>
-    ))}
+  //funcion para la peticion de un solo objeto y mostrar en reproducto de video
+  const fetchMovie = async(id) => {
+const {data} = await axios.get(`${API_URL}/movie/${id}`,{
+  params : {
+    api_key : API_KEY,
+    append_to_response:"videos"
+  }
+})
+if (data.videos && data.videos.results) {
+  const trailer = data.videos.results.find(
+    (vid) => vid.name === "Official Trailer"
+  )
+  setTrailer(trailer ? trailer : data.videos.results[0])
+}
+setMovie(data)
+} 
 
+  //funcion para buscar peliculas
+  const searchMovies = (e) => {
+    e.preventDefault();
+    fetchMovies(searchKey);
+  };
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+  return (
+    <div>
+      <h2 className="tex-center mt-5 mb-5">Trailer Movies</h2>
+      <form className="container mb-4" onSubmit={searchMovies}>
+        <input
+          type="text"
+          placeholder="search "
+          onChange={(e) => setSearchKey(e.target.value)}
+        />
+        <button className="btn btn primary">Search</button>
+      </form>
+      <div className="container mt-3">
+        <div className="row">
+          {movies.map((movie) => (
+            <div key={movie.id} className="col-md-4 mb-3">
+              <img
+                src={`${URL_IMAGE + movie.poster_path}`}
+                alt=""
+                height={600}
+                width="100%"
+              />
+              <h4 className="text-center">{movie.title}</h4>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  </div>
-  ) 
+  );
 }
 
 export default App;
